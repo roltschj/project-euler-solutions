cda107044ee676d0b77fc90ef8cf5af68277ca69
James Roltsch <68574801+roltschj@users.noreply.github.com>
2023-10-02 23:53:00 -0400

    add solution to problem 6

---
+++ 6-sum-square-difference.py
@@ -0,0 +1,22 @@
+# Find the difference between the sum of the squares of the first one hundred
+# natural numbers and the square of the sum.
+
+def getSumOfSquares(num):
+
+    result = 0
+
+    for i in range(num + 1):
+        result = result + i**2
+
+    return result
+
+def getSquareOfSum(num):
+
+    sum = 0
+
+    for i in range(num + 1):
+        sum = sum + i
+
+    return sum**2
+
+print(getSquareOfSum(10) - getSumOfSquares(100)) #25164150
